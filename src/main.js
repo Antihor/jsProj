401fc483b14665c139d4da4467bbb24131754fc3
Antihor <66antigor@gmail.com>
2025-11-22 15:28:53 +0200

    ddd

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const accordion = new Accordion('.faq-container', {
+  duration: 300,
+  showMultiple: true,
+});
